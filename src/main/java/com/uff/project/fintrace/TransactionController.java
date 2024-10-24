b39499d996135822a26de975f31d293325ad5d98
Gabriel Souza <gabriel.souza@ascsolutions.com.br>
2024-10-23 23:55:57 -0300

    Adicionando endpoints para realização de transação

--- src/main/java/com/uff/project/fintrace/TransactionController.java
+++ src/main/java/com/uff/project/fintrace/TransactionController.java
@@ -1,4 +1,48 @@
 package com.uff.project.fintrace;
 
+import com.uff.project.fintrace.model.Transaction;
+import com.uff.project.fintrace.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionController(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @GetMapping("/type/{type}")
+    public List<Transaction> getTransactionsByType(@PathVariable Transaction.Type type) {
+        return transactionRepository.findByType(type);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Transaction> getTransactionsByCategory(@PathVariable String category) {
+        return transactionRepository.findByCategory(category);
+    }
+
+    @GetMapping("/date-range")
+    public List<Transaction> getTransactionsByDateRange(
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalDate endDate) {
+        return transactionRepository.findByDateBetween(startDate, endDate);
+    }
 }
--- src/main/java/com/uff/project/fintrace/repository/TransactionRepository.java
+++ src/main/java/com/uff/project/fintrace/repository/TransactionRepository.java
@@ -1,4 +1,15 @@
 package com.uff.project.fintrace.repository;
 
-public interface TransactionRepository {
+import com.uff.project.fintrace.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByType(Transaction.Type type);
+
+    List<Transaction> findByCategory(String category);
+
+    List<Transaction> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
